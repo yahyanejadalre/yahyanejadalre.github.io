b34c02f3754a97afe5854d33bee2db66e6e07be2
unknown <yahyanejadalre@gmail.com>
2024-07-28 15:36:04 +0330

    dark mode added

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.querySelector('.nav-toggle'); // Updated class selector
-    const navMenu = document.querySelector('.nav-list'); // Updated class selector
+    const menuToggle = document.querySelector('.nav-toggle');
+    const navMenu = document.querySelector('.nav-list');
+    const darkModeToggle = document.querySelector('.dark-mode-toggle');
+    const body = document.body;
 
     menuToggle.addEventListener('click', function () {
-        navMenu.classList.toggle('active'); // Toggle 'active' class
+        navMenu.classList.toggle('active');
+    });
+
+    darkModeToggle.addEventListener('click', function () {
+        body.classList.toggle('dark-mode');
+        const icon = darkModeToggle.querySelector('i');
+        if (body.classList.contains('dark-mode')) {
+            icon.classList.replace('fa-moon', 'fa-sun');
+        } else {
+            icon.classList.replace('fa-sun', 'fa-moon');
+        }
     });
 });
